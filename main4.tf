bb04498289d21af35104518ddf8d27272fad141a
mannycepeda1989 <100322499+mannycepeda1989@users.noreply.github.com>
2023-10-02 11:03:54 -0700

    Create main4.tf

---
+++ main4.tf
@@ -0,0 +1,10 @@
+# main4.tf
+
+resource "aws_security_group_rule" "cluster_management_from_worker_nodes" {
+  security_group_id = aws_security_group.cluster_management.id
+  type             = "ingress"
+  from_port        = 443
+  to_port          = 443
+  protocol         = "TCP"
+  source_security_group_id = aws_security_group.worker_nodes.id
+}

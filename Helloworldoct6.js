96050ec5781a89c7eac7a4ac156f8c0cf5d9acef
mannycepeda1989 <100322499+mannycepeda1989@users.noreply.github.com>
2023-10-06 15:07:23 -0700

    Create Helloworldoct6.js

---
+++ Helloworldoct6.js
@@ -0,0 +1,8 @@
+// the hello world program
+alert("Hello, World!");
+
+const username = "johndoe";
+const password = "password123";
+
+// display the sum
+console.log('My username is: ' + username + ' and my password is: ' + password);

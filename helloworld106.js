da2cde587a689e58e2dac4dc32f61e7e9c63f6d0
mannycepeda1989 <100322499+mannycepeda1989@users.noreply.github.com>
2023-10-06 12:25:14 -0700

    Create helloworld106.js

---
+++ helloworld106.js
@@ -0,0 +1,8 @@
+// the hello world program
+alert("Hello World!");
+
+const username = "johnnydoe";
+const password = "password1234567";
+
+// display the sum
+console.log('My username is: ' + username + ' and my password is: ' + password);
